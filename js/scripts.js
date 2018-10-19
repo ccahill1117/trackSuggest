9ba64cdce9caa6d4ca43529492d4e6088e12b9fd
Student <example@example.com>
2018-10-19 10:06:40 -0700

    updated styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
 
-  //   $("button#takeQuiz").click(function() {
-  //     $("form#trackSubmit").show();
-  // });
+    $("button#takeQuiz").click(function() {
+      $("form#trackSubmit").show();
+  });
 
   $("form#trackSubmit").submit(function(event) {
     var day = parseInt($("#dayQuestion").val());
@@ -12,7 +12,7 @@ $(document).ready(function(){
     var continent = $("#continentQuestion").val();
     var player = $("#playerQuestion").val();
 
-    console.log(day, clas, meal, continent, player);
+    // console.log(day, clas, meal, continent, player);
 
     event.preventDefault();
 
