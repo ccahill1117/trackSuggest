d564828245aba6d992a14ba1b6222e32c229fe07
Student <example@example.com>
2018-10-19 12:07:49 -0700

    completed building if else structure

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 $(document).ready(function(){
 
 
-    $("button#takeQuiz").click(function() {
-      $("form#trackSubmit").show();
-  });
+  //   $("button#takeQuiz").click(function() {
+  //     $("form#trackSubmit").show();
+  // });
+
+  var addScores = function(no1, no2, no3, no4, no5) {
+    return no1 + no2 + no3 + no4 + no5;
+  };
 
   $("form#trackSubmit").submit(function(event) {
     var userName = $("#userName").val();
@@ -12,15 +16,11 @@ $(document).ready(function(){
     var meal = $("#mealQuestion").val();
     var continent = $("#continentQuestion").val();
     var player = $("#playerQuestion").val();
-    var userScore;
     var dayScore;
     var clasScore;
     var mealScore;
     var continentScore;
 
-    var addScores = function(no1, no2, no3, no4, no5) {
-      return no1 + no2 + no3 + no4 + no5;
-    };
 
 
     // console.log(userName, day, clas, meal, continent, player);
@@ -29,7 +29,9 @@ $(document).ready(function(){
       (clasScore = 5);
     } else if (clas === 'Science') {
       (clasScore = 2);
-    } else { }
+    } else {
+      (clasScore = 0);
+    }
 
     if (day >= 50) {
       (dayScore = 5);
@@ -37,15 +39,49 @@ $(document).ready(function(){
       (dayScore = 1);
     }
 
-    if (meal = 1) {
-      (mealScore = 1)
+    if (meal === 1) {
+      (mealScore = 1);
     } else {
       (mealScore = 0);
     }
 
-    
+    if (continent === 2) {
+        (continentScore = 2);
+    } else {
+      (continentScore = 0);
+    }
+
+    if (player > 15) {
+      (playerScore = 5);
+    } else if (player <= 15 && player >= 12) {
+      (playerScore = 2);
+    } else if (player < 12 && player > 0) {
+      (playerScore = 1);
+    } else {
+      (playerScore = 0);
+    }
+
+
+    var userScore = addScores(dayScore, clasScore, mealScore, continentScore, playerScore);
+
+
+
+    if (userScore >= 14) {
+      (course = "Ruby/React Nice Job, you're gonna do great ;-)");
+    }
+    else if (userScore >=8) {
+      (course = "CSS/Design alright good work");
+    }
+    else {
+      (course = "C#/.NET uh oh try again next time :-(");
+    }
+
+
+
+    $("#userResult").text(course);
 
     console.log(userScore);
+
 
     event.preventDefault();
 
