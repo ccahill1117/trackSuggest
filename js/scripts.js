85468398dcfbe228a240e3c4406cf87880d6a1bd
Student <example@example.com>
2018-10-19 09:58:29 -0700

    console log confirm getting values from input

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,18 @@ $(document).ready(function(){
   //     $("form#trackSubmit").show();
   // });
 
+  $("form#trackSubmit").submit(function(event) {
+    var day = parseInt($("#dayQuestion").val());
+    var clas = $("#classQuestion").val();
+    var meal = $("#mealQuestion").val();
+    var continent = $("#continentQuestion").val();
+    var player = $("#playerQuestion").val();
 
+    console.log(day, clas, meal, continent, player);
+
+    event.preventDefault();
+
+  });
 });
 
 
